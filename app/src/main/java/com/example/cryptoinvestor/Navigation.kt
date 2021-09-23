d1bf7952f3650fa3aba5e602d63915cf324db907
Kristoffer <s180500@win.dtu.dk>
2021-09-23 12:01:11 +0200

    tilføjet navigation bars navne

--- app/src/main/java/com/example/cryptoinvestor/Navigation.kt
+++ app/src/main/java/com/example/cryptoinvestor/Navigation.kt
@@ -26,7 +26,7 @@ class Navigation : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_portfolio, R.id.navigation_Crypto, R.id.navigation_Profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
