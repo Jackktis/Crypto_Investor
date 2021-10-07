8b9bdaf2e8f859a312a5c080d404c23ae7979437
Kristoffer <s180500@win.dtu.dk>
2021-10-07 13:04:56 +0200

    lavet at Login føre over in app. Mangler at få sign-up til at virke

--- app/src/main/java/com/example/cryptoinvestor/SplashScreen.kt
+++ app/src/main/java/com/example/cryptoinvestor/SplashScreen.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.text.TextUtils.replace
 import android.view.WindowManager
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -15,10 +19,17 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        Handler().postDelayed({
+
+        val loginButton = findViewById<Button>(R.id.BTLogin)
+        loginButton.setOnClickListener() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }
+        val signUpButton = findViewById<Button>(R.id.BTSignUp)
+        signUpButton.setOnClickListener() {
+
+        }
     }
+
 }
