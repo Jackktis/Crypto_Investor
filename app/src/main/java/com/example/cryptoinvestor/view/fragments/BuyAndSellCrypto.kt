ca71eca06b517f74d0ef612bfc2bbf8b88444c54
Kristoffer <s180500@win.dtu.dk>
2021-10-21 12:18:05 +0200

    lavet en BuyAndSellCrypto klasse/layout

--- app/src/main/java/com/example/cryptoinvestor/view/fragments/BuyAndSellCrypto.kt
+++ app/src/main/java/com/example/cryptoinvestor/view/fragments/BuyAndSellCrypto.kt
@@ -1,10 +1,11 @@
-package com.example.cryptoinvestor
+package com.example.cryptoinvestor.view.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cryptoinvestor.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BuyCrypto.newInstance] factory method to
+ * Use the [BuyAndSellCrypto.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BuyCrypto : Fragment() {
+class BuyAndSellCrypto : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class BuyCrypto : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buy_crypto, container, false)
+        return inflater.inflate(R.layout.fragment_buy_and_sell_crypto, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class BuyCrypto : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BuyCrypto.
+         * @return A new instance of fragment BuyAndSellCrypto.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BuyCrypto().apply {
+            BuyAndSellCrypto().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
---
+++ app/src/main/java/com/example/cryptoinvestor/view/fragments/BuyCryptoFragment.kt
@@ -0,0 +1,13 @@
+package com.example.cryptoinvestor.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BuyCryptoFragment : Fragment() {
+    
+
+
+}
