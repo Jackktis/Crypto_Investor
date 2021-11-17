dc732b1218f6ba782f35920c5c6fcf6a5d1f5592
MerithzDTU <s195480@student.dtu.dk>
2021-11-17 22:51:28 +0100

    Made changes to price formatting and made rate changes green or red according to positive or negative

--- app/src/main/java/com/example/cryptoinvestor/utils/formatters.kt
+++ app/src/main/java/com/example/cryptoinvestor/utils/formatters.kt
@@ -2,7 +2,7 @@ package com.example.cryptoinvestor.utils
 
 import java.text.DecimalFormat
 
-val PRICE_FORMATTER = DecimalFormat("$#,###.##")
+val PRICE_FORMATTER = DecimalFormat("#,###.##")
 val INTEGER_PRICE_FORMATTER = DecimalFormat("$#,###")
 val FLOAT_FORMATTER = DecimalFormat("0.00")
 val INTEGER_FORMATTER = DecimalFormat("#,###")
