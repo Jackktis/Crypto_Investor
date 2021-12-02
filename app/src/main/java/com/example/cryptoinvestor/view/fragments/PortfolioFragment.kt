e25e856ad1691f9ec63494db30b69abeb463e28b
MerithzDTU <s195480@student.dtu.dk>
2021-12-02 20:21:39 +0100

    Code merging, cleanup

--- app/src/main/java/com/example/cryptoinvestor/view/fragments/PortfolioFragment.kt
+++ app/src/main/java/com/example/cryptoinvestor/view/fragments/PortfolioFragment.kt
@@ -6,14 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.cryptoinvestor.databinding.FragmentPortfolioBinding
 import com.example.cryptoinvestor.utils.PRICE_FORMATTER
-import com.example.cryptoinvestor.view.adapter.PortfolioBoughtAdapter
 import com.example.cryptoinvestor.viewmodel.PortfolioViewModel
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_portfolio.*
@@ -24,7 +20,6 @@ class PortfolioFragment : Fragment() {
 
     private val portfolioViewModel: PortfolioViewModel by viewModels()
     private var binding: FragmentPortfolioBinding? = null
-    private val adapter by lazy { PortfolioBoughtAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,9 +35,9 @@ class PortfolioFragment : Fragment() {
         setupRecyclerViewForBought()
 
         TabLayoutMediator(tablayout, bought_currency_VP) { tab, position ->
-            if(position == 0){
+            if (position == 0) {
                 tab.setText("Favourites")
-            }else if (position == 1){
+            } else if (position == 1) {
                 tab.setText("Bought")
             }
         }.attach()
@@ -51,11 +46,11 @@ class PortfolioFragment : Fragment() {
         portfolioViewModel.userBalance.observe(viewLifecycleOwner, {
             view.UserBalancePortfolio.text = PRICE_FORMATTER.format(it.toFloat()).toString()
         })
-}
+    }
+
     private fun setupRecyclerViewForBought() {
         bought_currency_VP.adapter = Adapter(this)
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -65,11 +60,10 @@ class PortfolioFragment : Fragment() {
 
 class Adapter(parentFragment: Fragment) : FragmentStateAdapter(parentFragment) {
     override fun getItemCount(): Int = 2
-
     override fun createFragment(position: Int): Fragment {
-        if(position == 1) {
+        if (position == 1) {
             return BoughtFragment()
-        }else
+        } else
             return FavouritesFragment()
     }
 }
